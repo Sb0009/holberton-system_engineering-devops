9cb7a64120931f8725412b9ba9bcdb5266d75140
Sb0009 <phenissia2009@hotmail.fr>
2022-09-16 22:02:20 +0200

    1-

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,40 @@
 #!/usr/bin/python3
-"""" Python script that, using this REST API,
-for a given employee ID, returns information """
-
-import requests
-from sys import argv
-
+""" Gather data from an API  """
 
 if __name__ == "__main__":
-    userId = int(argv[1])
-    user = ('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
-    todo = ('{}/todos/'.format(user))
-    json_user = requests.get(user).json()
-    json_todo = requests.get(todo).json()
-    completed_tasks = []
+    from requests import get
+    from sys import argv, exit
 
-    for task in json_todo:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(json_user.get('name'), len(completed_tasks), len(json_todo)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+    try:
+        id = argv[1]
+        is_int = int(id)
+    except:
+        exit()
+
+    url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
+    url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
+
+    r_user = get(url_user)
+    r_todo = get(url_todo)
+
+    try:
+        js_user = r_user.json()
+        js_todo = r_todo.json()
+
+    except ValueError:
+        print("Not a valid JSON")
+
+    if js_user and js_todo:
+        EMPLOYEE_NAME = js_user[0].get('name')
+        TOTAL_NUMBER_OF_TASKS = len(js_todo)
+        NUMBER_OF_DONE_TASKS = sum(item.get("completed")
+                                   for item in js_todo if item)
+
+        print("Employee {} is done with tasks({}/{}):"
+              .format(EMPLOYEE_NAME,
+                      NUMBER_OF_DONE_TASKS,
+                      TOTAL_NUMBER_OF_TASKS))
+        for todo in js_todo:
+            TASK_TITLE = todo.get('title')
+            if todo.get("completed"):
+                print("\t {}".format(TASK_TITLE))
