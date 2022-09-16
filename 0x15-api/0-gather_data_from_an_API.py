150768c6c0b5f8d49a3eee38e27910f22cd2760c
Sb0009 <phenissia2009@hotmail.fr>
2022-09-16 21:51:32 +0200

    1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""" Python script that, using this REST API,
+for a given employee ID, returns information """
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    userId = int(argv[1])
+    user = ('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
+    todo = ('{}/todos/'.format(user))
+    json_user = requests.get(user).json()
+    json_todo = requests.get(todo).json()
+    completed_tasks = []
+
+    for task in json_todo:
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(json_user.get('name'), len(completed_tasks), len(json_todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
+
+
