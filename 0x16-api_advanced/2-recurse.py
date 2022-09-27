ee62f498af9fcefd7b33c4a89af0c12dac64f898
Sb0009 <phenissia2009@hotmail.fr>
2022-09-27 20:35:38 +0200

    require

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,14 +3,13 @@
 2-recurse
 """
 import requests
-import json
 
 
 def recurse(subreddit, hot_list=[], after=""):
     """Querying Reddit API, and returns all
     hot articles for a given subreddit."""
 
-    url = "https://api.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://api.reddit.com/r/{}/hot".json.format(subreddit)
     headers = {'User-Agent': 'sisi'}
     arg1 = {"limit": 100, "after": after}
     resp = requests.get(url, params=arg1, headers=headers)
@@ -25,3 +24,25 @@ def recurse(subreddit, hot_list=[], after=""):
         return hot_list
     else:
         return None
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """ Function recurse """
+    user_agent = {'User-agent': 'comels'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    req = requests.get(url,
+                       headers=user_agent,
+                       allow_redirects=False,
+                       params={'after': after})
+    if req.status_code == 404:
+        return None
+
+    hot = req.json()['data']
+    after = hot['after']
+
+    for result in hot['children']:
+        hot_list.append(result['data']['title'])
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
